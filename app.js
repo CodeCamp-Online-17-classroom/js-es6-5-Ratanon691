ad8fd22d422ea8b524245aeed2394793f30c6ac4
Non T. <117605943+Ratanon691@users.noreply.github.com>
2024-07-26 10:27:12 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// write code here
+function modifyArray(array1) {
+  const newArray = [...array1]
+  newArray[3] = newArray[3] ** 2
+  return newArray
+}
+
+const array1 = [1, 2, 3, 4, 5];
+const modifiedArray = modifyArray(array1);
+
+console.log(array1); // [1, 2, 3, 4, 5]
+console.log(modifiedArray); // [1, 2, 3, 16, 5]
